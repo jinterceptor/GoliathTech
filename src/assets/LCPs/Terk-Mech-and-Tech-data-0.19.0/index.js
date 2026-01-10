ccd81eb210ede6f5e1e043068b751431db37b102
jinterceptor <jensen.cocker@gmail.com>
2026-01-10 17:02:17 +0000

    Remove unused imports from index.js

---
+++ src/assets/LCPs/Terk-Mech-and-Tech-data-0.19.0/index.js
@@ -0,0 +1,12 @@
+"use strict"
+import f from "./frames.json"
+import s from "./systems.json"
+import w from "./weapons.json"
+
+const data = {
+  frames: f,
+  systems: s,
+  weapons: w,
+}
+
+export default data;
