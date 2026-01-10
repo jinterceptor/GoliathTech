9febed5b3627c2345c823ca9edf223605094ff59
jinterceptor <jensen.cocker@gmail.com>
2026-01-10 17:20:26 +0000

    Remove import of pilot_gear.json from index.js

---
+++ src/assets/LCPs/osr-data-1.2.0/index.js
@@ -0,0 +1,15 @@
+"use strict"
+import f from "./frames.json"
+import s from "./systems.json"
+import w from "./weapons.json"
+import t from "./talents.json"
+
+const data = {
+  frames: f,
+  pilot_gear: p,
+  systems: s,
+  weapons: w,
+  talents: t,
+}
+
+export default data;
